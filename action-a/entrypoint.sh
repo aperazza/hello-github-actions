835d4eb988927d85cdfa6eaab56fc22df5eecf32
Alexandre Perazza <alexandre.perazza@outlook.com>
2022-01-21 18:03:09 +0100

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo Hello World! My name is $INPUT_MY_NAME"
-sh -c "echo This workflow was executed with success on $DATE"
+sh -c "echo This workflow was executed with success on $INPUT_DATE"
